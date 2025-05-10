59feb3c3a9a6a284d53caf581ba89d4bd984126d
Nick Le Large <github@le-large.net>
2025-05-10 21:17:07 +0200

    Read a key sequence and print one of several hard-coded commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use crossterm::event::{read, Event, KeyCode, KeyEvent};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use std::collections::HashMap;
+
 fn main() {
-    print!("git status");
+    let mut shortcuts = HashMap::new();
+    shortcuts.insert("gs", ("git status", true));
+    shortcuts.insert("v", ("vim ", false));
+    shortcuts.insert("ll", ("ls -la", true));
+
+    let mut sequence = String::new();
+
+    enable_raw_mode().expect("Failed to enter raw mode");
+
+    loop {
+        if let Event::Key(KeyEvent { code, .. }) = read().expect("Failed to read event") {
+            match code {
+                KeyCode::Char(c) => {
+                    sequence.push(c);
+                    if let Some((cmd, execute_immediately)) = shortcuts.get(&sequence as &str) {
+                        if *execute_immediately {
+                            print!("#EXEC {}", cmd);
+                        } else {
+                            print!("{}", cmd); 
+                        }
+                        break;
+                    }
+                }
+                KeyCode::Esc => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    disable_raw_mode().expect("Failed to disable raw mode");
 }
+

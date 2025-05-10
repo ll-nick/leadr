0894710debe4dccf0e734c83cde25011b2dd71f4
Nick Le Large <github@le-large.net>
2025-05-10 18:11:22 +0200

    Small refactor of bash init script

--- shell/init.bash
+++ shell/init.bash
@@ -1,14 +1,22 @@
+# === Configurable Variables ===
+LEADR_BIND_KEY='\C-@' # Ctrl-Space
+LEADR_EXEC_PREFIX='#EXEC'
+LEADR_CMD_COLOR='\e[1;32m' # Bold green
+LEADR_RESET_COLOR='\e[0m'
+
+# === Handle leadr output ===
 __leadr_invoke__() {
-    local output
+    local cmd
     cmd="#EXEC git status"
 
-    if [[ "$cmd" =~ ^#EXEC[[:space:]]+(.*) ]]; then
-        # If prefixed with #EXEC, run the command right away and manually append to history
-        # Unfortunately, there is no easy way to simulate a user pressing enter, so this is the best we can do
+    if [[ "$cmd" =~ ^${LEADR_EXEC_PREFIX}[[:space:]]+(.*) ]]; then
+        # If using the exec prefix , run the command right away and manually append to history
+        # Unfortunately, there is no easy way to simulate a user pressing enter,
+        # so this is the best we can do without additional dependencies
         local actual_cmd="${BASH_REMATCH[1]}"
-        printf "\e[1;32m%s\e[0m\n" "$actual_cmd"
-        history -s "$actual_cmd" # Save to history
-        eval "$actual_cmd"       # Run in current shell
+        printf "${LEADR_CMD_COLOR}%s${LEADR_RESET_COLOR}\n" "$actual_cmd"
+        history -s "$actual_cmd"
+        eval "$actual_cmd"
     else
         # Otherwise, just prepare the command for the user to run
         READLINE_LINE="$cmd"
@@ -16,4 +24,5 @@ __leadr_invoke__() {
     fi
 }
 
-bind -x '"\C-@":__leadr_invoke__' # Ctrl-Space (C-@)
+# === Key Binding ===
+bind -x "\"${LEADR_BIND_KEY}\":__leadr_invoke__"

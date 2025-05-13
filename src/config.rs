8f644aad343209b1864d459f4081531fa89dd4c8
Nick Le Large <github@le-large.net>
2025-05-13 22:24:06 +0200

    Don't serialize some default values

--- src/config.rs
+++ src/config.rs
@@ -6,10 +6,19 @@ use crate::types::Shortcut;
 pub struct Config {
     #[serde(default = "default_leader")]
     pub leader: String,
-    #[serde(default = "default_exec_prefix")]
+
+    #[serde(
+        default = "default_exec_prefix",
+        skip_serializing_if = "is_default_exec_prefix"
+    )]
     pub exec_prefix: String,
-    #[serde(default = "default_padding")]
+
+    #[serde(
+        default = "default_padding",
+        skip_serializing_if = "is_default_padding"
+    )]
     pub padding: usize,
+
     pub shortcuts: HashMap<String, Shortcut>,
 }
 
@@ -40,49 +49,77 @@ fn default_leader() -> String {
 fn default_exec_prefix() -> String {
     "#EXEC".into()
 }
+fn is_default_exec_prefix(val: &str) -> bool {
+    val == default_exec_prefix()
+}
 
 fn default_padding() -> usize {
     4
 }
+fn is_default_padding(val: &usize) -> bool {
+    *val == default_padding()
+}
+
 
 impl ::std::default::Default for Config {
     fn default() -> Self {
         let mut shortcuts = HashMap::new();
-        shortcuts.insert("gs".into(), Shortcut {
-            command: "git status".into(),
-            description: Some("Git status".into()),
-            execute: true,
-        });
-        shortcuts.insert("ga".into(), Shortcut {
-            command: "git add .".into(),
-            description: Some("Git add all".into()),
-            execute: true,
-        });
-        shortcuts.insert("gc".into(), Shortcut {
-            command: "git commit -m \"".into(),
-            description: Some("Start a Git commit".into()),
-            execute: false,
-        });
-        shortcuts.insert("gp".into(), Shortcut {
-            command: "git push".into(),
-            description: Some("Git push".into()),
-            execute: true,
-        });
-        shortcuts.insert("gl".into(), Shortcut {
-            command: "git log --oneline".into(),
-            description: Some("Compact Git log".into()),
-            execute: true,
-        });
-        shortcuts.insert("h".into(), Shortcut {
-            command: "htop".into(),
-            description: Some("System monitor".into()),
-            execute: true,
-        });
-        shortcuts.insert("ip".into(), Shortcut {
-            command: "ip addr show".into(),
-            description: Some("Show IP addresses".into()),
-            execute: true,
-        });
+        shortcuts.insert(
+            "gs".into(),
+            Shortcut {
+                command: "git status".into(),
+                description: Some("Git status".into()),
+                execute: true,
+            },
+        );
+        shortcuts.insert(
+            "ga".into(),
+            Shortcut {
+                command: "git add .".into(),
+                description: Some("Git add all".into()),
+                execute: true,
+            },
+        );
+        shortcuts.insert(
+            "gc".into(),
+            Shortcut {
+                command: "git commit -m \"".into(),
+                description: Some("Start a Git commit".into()),
+                execute: false,
+            },
+        );
+        shortcuts.insert(
+            "gp".into(),
+            Shortcut {
+                command: "git push".into(),
+                description: Some("Git push".into()),
+                execute: true,
+            },
+        );
+        shortcuts.insert(
+            "gl".into(),
+            Shortcut {
+                command: "git log --oneline".into(),
+                description: Some("Compact Git log".into()),
+                execute: true,
+            },
+        );
+        shortcuts.insert(
+            "h".into(),
+            Shortcut {
+                command: "htop".into(),
+                description: Some("System monitor".into()),
+                execute: true,
+            },
+        );
+        shortcuts.insert(
+            "ip".into(),
+            Shortcut {
+                command: "ip addr show".into(),
+                description: Some("Show IP addresses".into()),
+                execute: true,
+            },
+        );
         Self {
             leader: default_leader(),
             exec_prefix: default_exec_prefix(),

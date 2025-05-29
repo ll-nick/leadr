595e4217ace995e08df437d45b96915ba450c553
Nick Le Large <github@le-large.net>
2025-05-29 21:48:06 +0200

    Remove some leftover structs
    These should have been removed when splitting types and errors

--- src/error.rs
+++ src/error.rs
@@ -1,29 +1,5 @@
 use thiserror::Error;
 
-#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
-pub struct Shortcut {
-    pub sequence: String,
-    pub command: String,
-    pub description: Option<String>,
-    pub execute: bool,
-}
-
-impl Shortcut {
-    pub fn format_command(&self, exec_prefix: &str) -> String {
-        if self.execute {
-            format!("{} {}", exec_prefix, self.command)
-        } else {
-            self.command.to_string()
-        }
-    }
-}
-
-pub enum ShortcutResult {
-    Shortcut(Shortcut),
-    Cancelled,
-    NoMatch,
-}
-
 #[derive(Debug, Error)]
 pub enum LeadrError {
     #[error("Conflicting sequence: {0}")]

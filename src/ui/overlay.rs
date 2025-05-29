7066463c3db77f8e712ca8c92634d0cb50d18dd5
Nick Le Large <github@le-large.net>
2025-05-29 23:04:34 +0200

    Implement drawFrame with multiple variants

--- src/ui/overlay.rs
+++ src/ui/overlay.rs
@@ -26,9 +26,8 @@ impl From<RgbColor> for crossterm::style::Color {
     }
 }
 
-#[derive(Clone, Default, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum BorderType {
-    #[default]
     Rounded,
     Square,
     Top,
@@ -39,17 +38,27 @@ pub enum BorderType {
 pub struct Config {
     pub height: u16,
     pub bg_color: RgbColor,
-    pub frame_color: RgbColor,
+    pub border_color: RgbColor,
     pub border: BorderType,
+    pub padding: u16,
 }
 
 impl std::default::Default for Config {
     fn default() -> Self {
         Self {
             height: 10,
-            bg_color: RgbColor { r: 49, g: 50, b: 68 },
-            frame_color: RgbColor { r: 137, g: 180, b: 250 },
+            bg_color: RgbColor {
+                r: 16,
+                g: 16,
+                b: 26,
+            },
+            border_color: RgbColor {
+                r: 137,
+                g: 180,
+                b: 250,
+            },
             border: BorderType::Rounded,
+            padding: 2,
         }
     }
 }
@@ -76,10 +85,7 @@ impl Overlay {
 
         tty.flush()?;
 
-        Ok(Self {
-            config,
-            scroll_up,
-        })
+        Ok(Self { config, scroll_up })
     }
 
     pub fn clear(&self) -> std::io::Result<()> {
@@ -106,41 +112,72 @@ impl Overlay {
         let (cols, rows) = terminal::size()?;
         let start_y = rows.saturating_sub(self.config.height);
 
+        self.draw_border(&mut tty, cols, start_y)?;
+
+        Ok(())
+    }
+
+    fn draw_border(&self, tty: &mut std::fs::File, cols: u16, start_y: u16) -> std::io::Result<()> {
+        let (top_left, top_right, bottom_left, bottom_right, horizontal, vertical) =
+            match self.config.border {
+                BorderType::Rounded => ('╭', '╮', '╰', '╯', '─', '│'),
+                BorderType::Square => ('┌', '┐', '└', '┘', '─', '│'),
+                BorderType::Top => ('─', '─', ' ', ' ', '─', ' '),
+                BorderType::None => (' ', ' ', ' ', ' ', ' ', ' '),
+            };
+
+        let padding = self.config.padding as usize;
+        let total_width = cols.saturating_sub(2) as usize;
+        let content_width = total_width.saturating_sub(padding * 2);
+        let horizontal_line = horizontal.to_string().repeat(content_width);
+
         tty.queue(cursor::SavePosition)?
-            .queue(cursor::MoveTo(0, start_y))?;
+            .queue(cursor::MoveTo(padding as u16, start_y))?;
 
-        let top = format!("╭{:─<col$}╮", "", col = cols.saturating_sub(2) as usize)
-            .with(Color::Rgb {
-                r: 0,
-                g: 128,
-                b: 255,
-            })
-            .on(Color::Rgb {
-                r: 10,
-                g: 10,
-                b: 50,
-            });
-        write!(tty, "{}", top)?;
+        // Top border
+        if !matches!(self.config.border, BorderType::None) {
+            let top = format!(
+                "{tl}{line}{tr}",
+                line = horizontal_line,
+                tl = top_left,
+                tr = top_right,
+            )
+            .with(self.config.border_color.into())
+            .on(self.config.bg_color.into());
+            write!(tty, "{}", top)?;
+        }
 
-        tty.queue(cursor::SavePosition)?
-            .queue(cursor::MoveTo(0, rows.saturating_sub(1)))?;
+        // Vertical sides
+        for i in 1..self.config.height {
+            tty.queue(cursor::MoveTo(padding as u16, start_y + i))?;
+            let line = format!(
+                "{vl}{space}{vr}",
+                space = " ".repeat(content_width),
+                vl = vertical,
+                vr = vertical
+            )
+            .with(self.config.border_color.into())
+            .on(self.config.bg_color.into());
+            write!(tty, "{}", line)?;
+        }
 
-        let bottom = format!("╰{:─<col$}╯", "", col = cols.saturating_sub(2) as usize)
-            .with(Color::Rgb {
-                r: 0,
-                g: 128,
-                b: 255,
-            })
-            .on(Color::Rgb {
-                r: 10,
-                g: 10,
-                b: 50,
-            });
-        write!(tty, "{}", bottom)?;
+        // Bottom border
+        if matches!(self.config.border, BorderType::Rounded | BorderType::Square) {
+            tty.queue(cursor::MoveTo(padding as u16, start_y + self.config.height))?;
+            let bottom = format!(
+                "{bl}{line}{br}",
+                line = horizontal_line,
+                bl = bottom_left,
+                br = bottom_right
+            )
+            .with(self.config.border_color.into())
+            .on(self.config.bg_color.into());
+            write!(tty, "{}", bottom)?;
+        }
 
         tty.queue(cursor::RestorePosition)?;
-
         tty.flush()?;
+
         Ok(())
     }
 }
